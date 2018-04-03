f0831ee3bfd567468c249e290e869b3c5c51c760
Mazen AZKAHE <ENGMAZENN93@Outlook.com>
2018-04-02 20:16:23 -0700

    Add project files.

---
+++ NumberVaribales/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberVaribales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = int.MinValue;
+            int x2 = int.MaxValue;
+            Console.WriteLine("int Min = " + x1 + "\nint Max = " + x2);
+            float f1 = float.MinValue;
+            float f2 = float.MaxValue;
+            Console.WriteLine("float Min = " + f1 + "\nfloat Max = " + f2);
+            Console.ReadKey();
+
+        }
+    }
+}
