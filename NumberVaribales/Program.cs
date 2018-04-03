7ea8ee72419e503700d3b5694d9ce99f24772d9e
Mazen AZKAHE <ENGMAZENN93@Outlook.com>
2018-04-02 20:30:15 -0700

    byte and decimal Added

--- NumberVaribales/Program.cs
+++ NumberVaribales/Program.cs
@@ -10,6 +10,9 @@ namespace NumberVaribales
     {
         static void Main(string[] args)
         {
+            byte b1 = byte.MinValue;
+            byte b2 = byte.MaxValue;
+            Console.WriteLine("byte Min = " + b1 + "\nbyte Max = " + b2);
             int x1 = int.MinValue;
             int x2 = int.MaxValue;
             Console.WriteLine("int Min = " + x1 + "\nint Max = " + x2);
@@ -19,6 +22,9 @@ namespace NumberVaribales
             double d1 = double.MinValue;
             double d2 = double.MaxValue;
             Console.WriteLine("double Min = " + d1 + "\ndouble Max = " + d2);
+            decimal de1 = decimal.MinValue;
+            decimal de2 = decimal.MaxValue;
+            Console.WriteLine("decimal Min = " + de1 + "\ndecimal Max = " + de2);
 
             Console.ReadKey();
 
